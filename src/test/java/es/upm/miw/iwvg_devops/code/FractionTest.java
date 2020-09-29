eac63f08d8390b00f83f8672b9d37e11836f76cc
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-09-29 21:08:37 +0200

    Update FractionTest.java #11

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -2,13 +2,56 @@ package es.upm.miw.iwvg_devops.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
 
     @Test
     public void testDecimal() {
         Fraction fraction = new Fraction(3, 2);
-        assertEquals(1.5, fraction.decimal(), 0.5);
+        assertEquals(1.5, fraction.decimal(), 10e-5);
     }
+
+    @Test
+    public void testIsProperWhenItShould() {
+        Fraction fraction = new Fraction(2, 3);
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    public void testIsNotProperWhenItShould() {
+        Fraction fraction = new Fraction(3, 2);
+        assertFalse(fraction.isProper());
+    }
+
+    @Test
+    public void testIsImproperWhenItShould() {
+        Fraction fraction = new Fraction(3, 2);
+        assertTrue(fraction.isImproper());
+    }
+
+    @Test
+    public void testIsNotImproperWhenItShould() {
+        Fraction fraction = new Fraction(2, 3);
+        assertFalse(fraction.isImproper());
+    }
+
+    @Test
+    public void testMultiply() {
+        Fraction fraction1 = new Fraction(3, 2);
+        Fraction fraction2 = new Fraction(2, 1);
+        Fraction multipliedFraction = fraction1.multiply(fraction2);
+
+        assertEquals(3.0, multipliedFraction.decimal(), 10e-5);
+    }
+
+    @Test
+    public void testDivide() {
+        Fraction fraction1 = new Fraction(3, 2);
+        Fraction fraction2 = new Fraction(3, 2);
+        Fraction dividedFraction = fraction1.divide(fraction2);
+
+        assertEquals(1.0, dividedFraction.decimal(), 10e-5);
+    }
+
 }
